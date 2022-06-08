d994735c3346451fa0548360466b87eb650e3d1d
DIP-POLLEY <dipanpolley2000@gmail.com>
2022-06-08 20:58:19 +0530

    status func 1

---
+++ lib/screens/statusview.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:story_view/controller/story_controller.dart';
+import 'package:story_view/story_view.dart';
+
+class Statusview extends StatelessWidget {
+   Statusview({Key? key}) : super(key: key);
+
+
+  final controller = StoryController();
+
+  @override
+  Widget build(BuildContext context) {
+    List<StoryItem> stories =  [
+      StoryItem.text(
+        title: "fdfssf",
+        backgroundColor: Colors.amberAccent
+      ),
+      StoryItem.text(
+          title: "fdfssf",
+          backgroundColor: Colors.redAccent
+      ),
+      StoryItem.text(
+          title: "fdfssf",
+          backgroundColor: Colors.cyanAccent
+      ),
+      StoryItem.text(
+          title: "fdfssf",
+          backgroundColor: Colors.deepPurple
+      ),
+      StoryItem.text(
+          title: "fdfssf",
+          backgroundColor: Colors.teal
+      ),
+
+
+
+    ];
+    return StoryView(
+        storyItems: stories,
+        controller: controller, // pass controller here too
+        repeat: false, // should the stories be slid forever
+        //onStoryShow: (s) {notifyServer(s)},
+        onComplete: () {
+          Navigator.pop(context);
+        },
+        onVerticalSwipeComplete: (direction) {
+          if (direction == Direction.down) {
+            Navigator.pop(context);
+          }
+        } // To disable vertical swipe gestures, ignore this parameter.
+      // Preferrably for inline story view.
+    );
+  }
+}
--- lib/widgets/statusfeedcard.dart
+++ lib/widgets/statusfeedcard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/screens/statusview.dart';
 import 'package:whatsapp/utilities/constants.dart';
 
 
@@ -12,59 +13,67 @@ class Statusfeedcard extends StatefulWidget {
 class _StatusfeedcardState extends State<Statusfeedcard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      height: MediaQuery.of(context).size.height *1.5 / kTabSize,
-      child: Row(
-        children: [
-          Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 15),
-                child: CircleAvatar(
-                  radius: 22,
-                  backgroundImage: AssetImage("assets/img.png"),
-                ),
-              )
-          ),
+    return GestureDetector(
+      onTap: ()
+      {
+          Navigator.push(context,
+          MaterialPageRoute(builder: (context) => Statusview())
+          );
+      },
+      child: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height *1.5 / kTabSize,
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: CircleAvatar(
+                    radius: 22,
+                    backgroundImage: AssetImage("assets/img.png"),
+                  ),
+                )
+            ),
 
-          Expanded(
-              flex: 8,
-              child: Padding(
-                padding: const EdgeInsets.only(left:10,top: 12 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Padding(
-                      padding:  EdgeInsets.all(4.0),
-                      child: Text(
-                        "Contact Name",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
+            Expanded(
+                flex: 8,
+                child: Padding(
+                  padding: const EdgeInsets.only(left:10,top: 12 ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Padding(
+                        padding:  EdgeInsets.all(4.0),
+                        child: Text(
+                          "Contact Name",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding:  EdgeInsets.all(4.0),
-                      child: Text(
-                        "Yesterday, 5:78 pm",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 15,
-                            color: Colors.black45
+                      Padding(
+                        padding:  EdgeInsets.all(4.0),
+                        child: Text(
+                          "Yesterday, 5:78 pm",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                              color: Colors.black45
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-          ),
+                    ],
+                  ),
+                )
+            ),
 
 
 
-        ],
+          ],
+        ),
       ),
     );
   }
