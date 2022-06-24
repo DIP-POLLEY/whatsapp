d0b118e5a6d23b8625ed8342f9f7364e745c4f0d
Kumar-Rishabh <kr.rishabh442@gmail.com>
2022-06-25 00:20:45 +0530

    chat screen added

--- lib/widgets/chatfeedcard.dart
+++ lib/widgets/chatfeedcard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/utilities/constants.dart';
 
+import '../screens/chatscreen.dart';
+
 class ChatFeedCard extends StatelessWidget {
   final bool unread;
   final bool mute;
@@ -17,96 +19,106 @@ class ChatFeedCard extends StatelessWidget {
     return Container(
       color: Colors.white,
       height: MediaQuery.of(context).size.height *1.75 / kTabSize,
-      child: Row(
-        // ignore_for_file: prefer_const_constructors
-        children: [
-          Expanded(
-            flex: 2,
-              child: Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: CircleAvatar(
-                radius: 22,
-                backgroundImage: AssetImage("assets/img.png"),
-              ),
-            )
-          ),
-
-          Expanded(
-            flex: 6,
-              child: Padding(
-                padding: const EdgeInsets.only(left:10,top: 15 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Padding(
-                      padding:  EdgeInsets.all(4.0),
-                      child: Text(
-                        "Contact Name",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding:  EdgeInsets.all(4.0),
-                      child: Text(
-                        "Last Chat",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 15,
-                            color: Colors.black45
-                        ),
-                      ),
-                    ),
-                  ],
+      child: GestureDetector(
+        onTap: (){
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => ChatScreen(),
+            ),
+          );
+        },
+        child: Row(
+          // ignore_for_file: prefer_const_constructors
+          children: [
+            Expanded(
+              flex: 2,
+                child: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: CircleAvatar(
+                  radius: 22,
+                  backgroundImage: AssetImage("assets/img.png"),
                 ),
               )
-          ),
+            ),
 
-
-          Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Text(
-                      "8:48 pm"
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0,top: 10),
-                  child: Row(
-                    children: [
-
-                      mute==true?Icon(
-                          Icons.volume_mute_outlined,
-                      ):Text("       "),
-                      unread==true?
-                      Container(
-                        height: 20,
-                          width: 20,
-                          decoration: BoxDecoration(
-                             color: Colors.greenAccent,
-                              borderRadius: BorderRadius.all(Radius.circular(20))
+            Expanded(
+              flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.only(left:10,top: 15 ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Padding(
+                        padding:  EdgeInsets.all(4.0),
+                        child: Text(
+                          "Contact Name",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18
                           ),
-                          child: Text(
-                            "3",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white
-                            ),
+                        ),
+                      ),
+                      Padding(
+                        padding:  EdgeInsets.all(4.0),
+                        child: Text(
+                          "Last Chat",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 15,
+                              color: Colors.black45
                           ),
-                      ):Text(""),
+                        ),
+                      ),
                     ],
                   ),
                 )
-              ],
             ),
-          )
-        ],
+
+
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Text(
+                        "8:48 pm"
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0,top: 10),
+                    child: Row(
+                      children: [
+
+                        mute==true?Icon(
+                            Icons.volume_mute_outlined,
+                        ):Text("       "),
+                        unread==true?
+                        Container(
+                          height: 20,
+                            width: 20,
+                            decoration: BoxDecoration(
+                               color: Colors.greenAccent,
+                                borderRadius: BorderRadius.all(Radius.circular(20))
+                            ),
+                            child: Text(
+                              "3",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white
+                              ),
+                            ),
+                        ):Text(""),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
